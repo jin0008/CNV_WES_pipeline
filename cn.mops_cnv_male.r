2e9eef19972beb5ba49656ed39ad75a27c427d71
juugeebee <july2023@yahoo.fr>
2020-07-13 16:14:22 +0200

    adresse target XY

--- cn.mops_cnv_male.r
+++ cn.mops_cnv_male.r
@@ -2,7 +2,7 @@
 
 library(cn.mops)
 
-segments_XY <- read.table(file="gencode.v34.basic.annotation.XY.scratch.bed",
+segments_XY <- read.table(file="/media/Data1/jbogoin/ref/gencode/v34_hg38/XY/gencode.v34.basic.annotation.XY.scratch.bed",
                     header=FALSE, sep="\t", as.is=TRUE)
 
 
