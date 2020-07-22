257a055355004300de883f7b0d0c4ab3e0ac3a6e
jin0008 <jin0008@hanmail.net>
2020-07-22 23:53:54 +0900

    Update pipeline_cnv.sh

--- pipeline_cnv.sh
+++ pipeline_cnv.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Author: Julie BOGOIN
+# Modified by: Jinu Han
 
 exec &> cnv_sortie.log
 
@@ -15,48 +16,48 @@ source ~/miniconda3/etc/profile.d/conda.sh
 # sex determination
 conda activate sex_env
 
-python3 ~/CNV_WES_pipeline/sex_determination.py
+python3 /home/hanjinu/src/CNV_WES_pipeline/sex_determination.py
 
 conda deactivate
 
 # generation des listes hommes et femmes
 conda activate results_cnv
 
-python ~/CNV_WES_pipeline/female_male_lists.py
+python /home/hanjinu/src/CNV_WES_pipeline/female_male_lists.py
 
 conda deactivate
 
 # tools cnv detection
-bash ~/CNV_WES_pipeline/cn.mops_launch.sh
-bash ~/CNV_WES_pipeline/cnvkit_detection.sh
-bash ~/CNV_WES_pipeline/excavator2_detection.sh
-bash ~/CNV_WES_pipeline/exomedepth_launch.sh
-bash ~/CNV_WES_pipeline/gatk_detection.sh
+bash /home/hanjinu/src/CNV_WES_pipeline/cn.mops_launch.sh
+bash /home/hanjinu/src/CNV_WES_pipeline/cnvkit_detection.sh
+bash /home/hanjinu/src/CNV_WES_pipeline/excavator2_detection.sh
+bash /home/hanjinu/src/CNV_WES_pipeline/exomedepth_launch.sh
+bash /home/hanjinu/src/CNV_WES_pipeline/gatk_detection.sh
 
 # tools results generation
 conda activate results_cnv
 
 cd cn.mops_output
-python ~/CNV_WES_pipeline/cn.mops_results.py
+python /home/hanjinu/src/CNV_WES_pipeline/cn.mops_results.py
 
 cd ../cnvkit_output
-python ~/CNV_WES_pipeline/cnvkit_results.py
+python /home/hanjinu/src/CNV_WES_pipeline/cnvkit_results.py
 
 cd ../excavator2_output
-python ~/CNV_WES_pipeline/excavator2_results.py
+python /home/hanjinu/src/CNV_WES_pipeline/excavator2_results.py
 
 cd ../exomedepth_output
-python ~/CNV_WES_pipeline/exomedepth_results.py
+python /home/hanjinu/src/CNV_WES_pipeline/exomedepth_results.py
 
 cd ../gatkcnv_output
-python ~/CNV_WES_pipeline/gatk_results.py
+python /home/hanjinu/src/CNV_WES_pipeline/gatk_results.py
 
 cd $DATA
 
 # results summary
-python ~/CNV_WES_pipeline/cnv_results.py
-python ~/CNV_WES_pipeline/cnv_interval_objet_sample.py
-python ~/CNV_WES_pipeline/cnv_interval_objet_run.py
+python /home/hanjinu/src/CNV_WES_pipeline/cnv_results.py
+python /home/hanjinu/src/CNV_WES_pipeline/cnv_interval_objet_sample.py
+python /home/hanjinu/src/CNV_WES_pipeline/cnv_interval_objet_run.py
 
 conda deactivate
 
@@ -65,21 +66,21 @@ conda deactivate
 # annovar
 conda activate annot_env
 
-bash ~/CNV_WES_pipeline/annovar.sh
+bash /home/hanjinu/src/CNV_WES_pipeline/annovar.sh
 
 conda deactivate
 
 conda activate results_cnv
 
 cd annovar_output
-python ~/CNV_WES_pipeline/annovar_results.py
+python /home/hanjinu/src/CNV_WES_pipeline/annovar_results.py
 
 # ClinVar
 # In_gene
 # DGV_count
 
 cd $DATA
-python ~/CNV_WES_pipeline/combine_annot.py
+python /home/hanjinu/src/CNV_WES_pipeline/combine_annot.py
 
 conda deactivate
 
