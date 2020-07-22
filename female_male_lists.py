d81f627a241f60dcb4430a9f5ed207c59b1216bd
jin0008 <jin0008@hanmail.net>
2020-07-22 20:07:49 +0900

    Update female_male_lists.py

--- female_male_lists.py
+++ female_male_lists.py
@@ -14,7 +14,7 @@ else:
     if os.path.isfile('samples.txt'):
         with open ('samples.txt', 'r') as file:
             for line in file:
-                line = line.split(sep='\n')
+                line = f.readline()
                 info = line[0].split(sep='\t')
                 if info[1]=='M':
                     males.append(info[0])
