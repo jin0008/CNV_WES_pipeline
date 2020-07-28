b55b5006b176bfbf5854c54b3360808b827ae869
jin0008 <jin0008@hanmail.net>
2020-07-28 19:08:58 +0900

    Update gatk_results.py

--- gatk_results.py
+++ gatk_results.py
@@ -9,7 +9,7 @@ import gzip
 ###########
 #FUNCTIONS#
 ###########
-
+start=('#', '##')
 VCF_HEADER = ['contig', 'start', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER', 'end', 'FORMAT', 'CN']
 
 def _count_comments(filename):
