626287494165070cd7dd63493e5c5dcb91023fa8
jin0008 <jin0008@hanmail.net>
2020-08-08 13:05:49 +0900

    Update cn.mops_launch.sh

--- cn.mops_launch.sh
+++ cn.mops_launch.sh
@@ -10,9 +10,9 @@ echo ""
 rm -Rf cn.mops_output
 mkdir cn.mops_output
 
-R -q --vanilla < /home/hanjinu/src/CNV_WES_pipeline/cn.mops_cnv_female.r
-R -q --vanilla < /home/hanjinu/src/CNV_WES_pipeline/cn.mops_cnv_male.r
-R -q --vanilla < /home/hanjinu/src/CNV_WES_pipeline/cn.mops_cnv_all.r
+R -q --vanilla < /home/hanjinu/src/CNV_WGS_pipeline/cn.mops_cnv_female.r
+R -q --vanilla < /home/hanjinu/src/CNV_WGS_pipeline/cn.mops_cnv_male.r
+R -q --vanilla < /home/hanjinu/src/CNV_WGS_pipeline/cn.mops_cnv_all.r
 
 echo ""
 echo "cn.mops CNV DETECTION job done!"
