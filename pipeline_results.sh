fb3b500eb9b364ea86dc1c295b473c165947ad97
jin0008 <jin0008@hanmail.net>
2020-09-16 09:25:16 +0900

    Update pipeline_results.sh

--- pipeline_results.sh
+++ pipeline_results.sh
@@ -7,30 +7,30 @@ echo "pipeline_results.sh start"
 echo ""
 
 # # # results summary
-python ~/CNV_WES_pipeline/cnv_results.py
-python ~/CNV_WES_pipeline/cnv_interval_objet_sample.py
-python ~/CNV_WES_pipeline/cnv_interval_objet_run.py
+python /home/hanjinu/src/CNV_WES_pipeline/cnv_results.py
+python /home/hanjinu/src/CNV_WES_pipeline/cnv_interval_objet_sample.py
+python /home/hanjinu/src/CNV_WES_pipeline/cnv_interval_objet_run.py
 
-python ~/CNV_WES_pipeline/frequences.py
+python /home/hanjinu/src/CNV_WES_pipeline/frequences.py
 
 # annotations
 
 # annovar
  conda activate annot_env
 
-bash ~/CNV_WES_pipeline/annovar.sh
+bash /home/hanjinu/src/CNV_WES_pipeline/annovar.sh
 
 conda activate results_cnv
 
 cd annovar_output
-python ~/CNV_WES_pipeline/annovar_results.py
+python /home/hanjinu/src/CNV_WES_pipeline/annovar_results.py
 
 # ClinVar
 # In_gene
 # DGV_count
 
 cd $DATA
-python ~/CNV_WES_pipeline/combine_annot.py
+python /home/hanjinu/src/CNV_WES_pipeline/combine_annot.py
 
 echo ""
 echo "pipeline_results.sh job done!"
