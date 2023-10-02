e0b9eb3c61c7b8a17dde1089245a61a9603d3458
jin0008 <jin0008@hanmail.net>
2023-10-02 10:55:49 -0400

    Update exomedepth_launch.sh

--- exomedepth_launch.sh
+++ exomedepth_launch.sh
@@ -1,21 +1,24 @@
 # Author: Julie BOGOIN
 
 source ~/miniconda3/etc/profile.d/conda.sh
-conda activate
+conda activate exomedepth_env
 
 echo ""
-echo "ExomeDepth CNV DETECTION start"
+echo "*******************************"
+echo "ExomeDepth CNV DETECTION start."
+echo "*******************************"
 echo ""
 
-rm -Rf exomedepth_output
-mkdir exomedepth_output
-cd exomedepth_output
-mkdir all
-mkdir female
-mkdir male
-cd ..
 
-R -q --vanilla < /home/hanjinu/src/CNV_WES_pipeline/exomedepth_cnv.r
+# rm -Rf exomedepth_output
+# mkdir exomedepth_output
+# cd exomedepth_output
+# mkdir all
+# mkdir female
+# mkdir male
+# cd ..
+
+R -q --vanilla < ~/SCRIPTS/CNV_WES_pipeline/exomedepth_cnv.r
 
 echo ""
 echo "ExomeDepth CNV DETECTION job done!"
