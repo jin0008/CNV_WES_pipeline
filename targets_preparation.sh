6a183a26ddccf23ba6b25fc3d5659d965b63d219
jin0008 <jin0008@hanmail.net>
2023-10-02 12:01:03 -0400

    Update targets_preparation.sh

--- targets_preparation.sh
+++ targets_preparation.sh
@@ -11,31 +11,31 @@ echo ""
 
 DIC="/media/hanjinu/PM883/db/refs/hg38_broad/Homo_sapiens_assembly38.dict"
 
-# Télécharger le fichier gencode.v34.basic.annotation.gff3 sur le site de GENECODE
-#wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_34/gencode.v34.basic.annotation.gff3.gz
+# Télécharger le fichier gencode.v44.basic.annotation.gff3 sur le site de GENECODE
+#wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_44/gencode.v44.basic.annotation.gff3.gz
 
 # Recupérer les CDS
-zgrep "CDS" gencode.v34.basic.annotation.gff3.gz > gencode.v34.basic.annotation.CDS.gff3
+zgrep "CDS" gencode.v44.basic.annotation.gff3.gz > gencode.v44.basic.annotation.CDS.gff3
 
 # Ajouter une 4ème colonne et trier
-awk -F "\t" '{print $1"\t"$4-1"\t"$5}' gencode.v34.basic.annotation.CDS.gff3 \
-    | sort -Vu > gencode.v34.basic.annotation.CDS.bed
+awk -F "\t" '{print $1"\t"$4-1"\t"$5}' gencode.v44.basic.annotation.CDS.gff3 \
+    | sort -Vu > gencode.v44.basic.annotation.CDS.bed
 
 # Supprimer les intervalles chevauchants
-bedtools merge -i gencode.v34.basic.annotation.CDS.bed > gencode.v34.basic.annotation.CDS.merged.bed 
+bedtools merge -i gencode.v44.basic.annotation.CDS.bed > gencode.v44.basic.annotation.CDS.merged.bed 
 
 # Ajouter une colonne 4
-awk -F "\t" '{print $1, $2, $3, "CDS"}' gencode.v34.basic.annotation.CDS.merged.bed > gencode.v34.basic.annotation.CDS.merged.4fields.bed
+awk -F "\t" '{print $1, $2, $3, "CDS"}' gencode.v44.basic.annotation.CDS.merged.bed > gencode.v44.basic.annotation.CDS.merged.4fields.bed
 
 # Transformer bed en interval.list
 gatk BedToIntervalList \
-    -I gencode.v34.basic.annotation.CDS.merged.4fields.bed \
-    -O gencode.v34.basic.annotation.CDS.merged.4fields.interval_list \
-    -SD /media/hanjinu/PM883/db/refs/hg38/resources_broad_hg38_v0_Homo_sapiens_assembly38.dict
+    -I gencode.v44.basic.annotation.CDS.merged.4fields.bed \
+    -O gencode.v44.basic.annotation.CDS.merged.4fields.interval_list \
+    -SD /media/hanjinu/PM883/db/refs/hg38_broad/Homo_sapiens_assembly38.dict
 
 # Faire un fichier cible par chromosome
 for i in {1..22} X Y; do ;
-grep "^chr${i}" gencode.v34.basic.annotation.CDS.merged.bed > gencode.v34.basic.annotation.CDS.chr${i}.bed; 
+grep "^chr${i}" gencode.v44.basic.annotation.CDS.merged.bed > gencode.v44.basic.annotation.CDS.chr${i}.bed; 
 done
 
 echo ""
