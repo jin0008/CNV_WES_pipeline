7dddfeb5376fca34e33fca0d002c4c7ff522e6de
jin0008 <jin0008@hanmail.net>
2023-10-02 11:02:17 -0400

    Update sex_determination.py

--- sex_determination.py
+++ sex_determination.py
@@ -18,9 +18,11 @@ for i in bams:
     
     sample = os.path.basename(i.split(".")[0])
     sample_l.append(sample)
-    bamfile = pysam.AlignmentFile(i, "rb")
+    bamfile = pysam.AlignmentFile(i, "rb", check_sq=False)
     sry_count = bamfile.count(contig='chrY', start=2786989, stop=2787603, until_eof=False, read_callback='all')
-    
+    print(sry_count)
+
+
     if sry_count >= 50:
         sex = "M"
     elif sry_count <= 10:
