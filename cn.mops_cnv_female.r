10533b56777ce739d623d0732772eb8a582dd451
jin0008 <jin0008@hanmail.net>
2023-10-11 20:10:20 -0400

    Update cn.mops_cnv_female.r

--- cn.mops_cnv_female.r
+++ cn.mops_cnv_female.r
@@ -19,7 +19,7 @@ setwd(dir=".")
 
 gr <- GRanges(segments_XY[,1], IRanges(segments_XY[,2],segments_XY[,3]))
 
-X <- getSegmentReadCountsFromBAM(female[,1], GR=gr)
+X <- getSegmentReadCountsFromBAM(female[,1], GR=gr, parallel=32)
 
 resCNMOPS <- exomecn.mops(X, lowerThreshold = -0.9)
 
